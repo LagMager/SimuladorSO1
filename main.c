43a66b0896161aa2c5c4302a17d40b0ba55f8537
Juanjo200 <131538215+Juanjo200@users.noreply.github.com>
2025-03-16 16:02:17 -0500

    Creando registros

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MEM_SIZE 256
+#define LINE_SIZE 50
+
+int memory[MEM_SIZE] = {0}; 
+int ACC = 0;               
+int ICR = 0;               
+int MAR = 0;                
+int MDR = 0;                
+int UC = 1;                 // Unidad de Control
+
